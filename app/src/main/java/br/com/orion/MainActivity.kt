48f8c79c3a0338564a8af8e42a3bcb2b0f34ee71
iurip <iuripadilha827@gmail.com>
2025-07-03 17:51:18 -0300

    Orion - Adicionando o publishing.gradle

--- app/src/main/java/br/com/orion/MainActivity.kt
+++ app/src/main/java/br/com/orion/MainActivity.kt
@@ -4,14 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import br.com.orion.ui.theme.OrionTheme
+import br.com.design.theme.OrionTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +12,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OrionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OrionTheme {
-        Greeting("Android")
     }
 }
--- design/theme/src/main/java/br/com/design/theme/Color.kt
+++ design/theme/src/main/java/br/com/design/theme/Color.kt
@@ -1,4 +1,4 @@
-package br.com.orion.ui.theme
+package br.com.design.theme
 
 import androidx.compose.ui.graphics.Color
 
